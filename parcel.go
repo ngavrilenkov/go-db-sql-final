125b1e835504746e2ed6cd1f22027b82ea2f79ef
Nick N <ngavrilenkov@gmail.com>
2024-02-17 15:18:29 +0300

    remove comment

--- parcel.go
+++ parcel.go
@@ -14,7 +14,6 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 }
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
-	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
 	query := "INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, :address, :created_at)"
 
 	res, err := s.db.Exec(query,
